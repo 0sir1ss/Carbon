02abcb798ea6da7b88e00e8df9585739437d911f
0sir1ss <86688779+0sir1ss@users.noreply.github.com>
2022-05-31 23:53:31 +0000

    Delete examples directory

--- examples/script-obf.py
+++
@@ -1,9 +0,0 @@
-def lIIlIllIIllI(IllllIIlI, IlIIlIlII, IIIIIIlllllI='arg3', *IIIlIllIl, IIlIIIIlI, **IIIIlIll):
-    print('arg1', IllllIIlI)
-    print('arg2', IlIIlIlII)
-    print('arg3', IIIIIIlllllI)
-    print('arg4', IIIlIllIl)
-    print('arg5', IIlIIIIlI)
-    print('kwargs', IIIIlIll)
-IlIIlIIIIlllIlIllIl = "hi"
-lIIlIllIIllI('a', f'b: {IlIIlIIIIlllIlIllIl}', IIIIIIlllllI='c', IIIlIllIl='d', IIlIIIIlI='e')
